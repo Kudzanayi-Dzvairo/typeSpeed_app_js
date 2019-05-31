eb98ca43de90d2c444d4fb855e859c3a0befe333
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-05-30 23:20:35 -0400

    working on timer and score

---
+++ app.js
@@ -0,0 +1,101 @@
+window.addEventListener('load', init)
+//Global Variables
+let time = 5;
+let score = 0;
+let isPlaying;
+
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#secods');
+
+const words = [
+    'hat',
+  'river',
+  'lucky',
+  'statue',
+  'generate',
+  'stubborn',
+  'cocktail',
+  'runaway',
+  'joke',
+  'developer',
+  'establishment',
+  'hero',
+  'javascript',
+  'nutrition',
+  'revolver',
+  'echo',
+  'siblings',
+  'investigate',
+  'horrendous',
+  'symptom',
+  'laughter',
+  'magic',
+  'master',
+  'space',
+  'definition'
+];
+
+//Initialize Game
+function init(){
+    //Load word form array
+    showWord(words);
+    //start matching on word input 
+    wordInput.addEventListener('input', startMatch)
+    //Call countdown every second
+    setInterval(countdown, 1000);
+    //check game status
+    setInterval(checkStatus, 50);
+}
+
+function startMatch(){
+    if(matchWords()){
+        isPlaying = true;
+        time = 6;
+        showWord(words);
+        wordInput.value ='';
+        score++;
+    }
+    scoreDisplay.innerHTML = score
+}
+
+function matchWords(){
+    if(wordInput.value === currentWord.innerHTML){
+        message.innerHTML = 'Correct!!!';
+        return true;
+    }else{
+        message.innerHTML = '';
+        return false;
+    }
+}
+
+//Pick and show random word
+function showWord(words){
+//Gnerate random array index
+    const randIndex = Math.floor(Math.random() * words.length);
+//Outout random word
+    currentWord.innerHTML = words[randIndex]
+}
+
+function countdown(){
+    //make sure time is not run out 
+    if(time > 0){
+        //Decrement
+        time --
+    }else if(time === 0){
+        isPlaying = false;
+    }
+
+    timeDisplay.innerHTML = time;
+}
+
+//Check game status 
+function checkStatus(){
+    if(!isPlaying && time === 0){
+        message.innerHTML = 'Game Over Bitch!!!'
+        score = -1;
+    }
+}
